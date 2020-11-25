50244811788a530cae8c32883436da848a1d603d
Sergej ZHigulyov <satmat2013zhig@gmail.com>
2020-11-25 21:42:53 +0700

    fix parse created time to string

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -3,21 +3,22 @@ import { formatDistanceToNow } from 'date-fns'
 
 import './task.css';
 
-const Task = ({
-    onDelete: deleteBtnClickHandler,
-    onCompleted: labelClickHandler,
-    task: {
-        description = 'Task description',
-        created,
-        editing = false
-    }
-}) => {
-    let createdString = '';
-    let createdDate = created ? new Date(created) : null;
+const Task = (props) => {
+    const {
+        onDelete: deleteBtnClickHandler,
+        onCompleted: labelClickHandler,
+        task: {
+            description = 'Task description',
+            created,
+            editing = false
+        }
+    } = props;
 
-    if (createdDate && createdDate.toString() !== 'Invalid Date') {
-        createdString = `created ${ formatDistanceToNow(createdDate) } ago`;
-    }
+    const getCreatedTime = () => {
+        if (!created instanceof Date) return '';
+
+        return `created ${ formatDistanceToNow(created) } ago`;
+    };
 
     const EditInput = () => editing ? <input type="text" className="edit" defaultValue={description} /> : null;
 
@@ -27,7 +28,7 @@ const Task = ({
                 <input className="toggle" type="checkbox" />
                 <label onClick={ labelClickHandler }>
                     <span className="description">{ description }</span>
-                    <span className="created">{ createdString }</span>
+                    <span className="created">{ getCreatedTime() }</span>
                 </label>
                 <button className="icon icon-edit"></button>
                 <button onClick={ deleteBtnClickHandler } className="icon icon-destroy"></button>
